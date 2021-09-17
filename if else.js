8f056f8ba276a582fc80a7abee7a0017bdf7c99e
Azkorb <87208730+Azkorb@users.noreply.github.com>
2021-09-17 08:31:19 -0700

    Add files via upload

---
+++ if else.js
@@ -0,0 +1,18 @@
+var nombre = prompt("cual es tu nombre?"),
+            edad = 24;
+
+        if(nombre === "pedro") {
+            console.log("Bienbenido "+nombre);
+        } else {
+            console.log("no te conosco quien eres?, Ayuda!!!");
+        }
+
+/*
+
+if (condición) {
+  código a ejecutar si la condición es verdadera
+} else {
+  ejecuta este otro código si la condición es falsa
+}
+
+*/
---
+++ if.js
@@ -0,0 +1,7 @@
+var edad = 15;
+if(edad >= 18){
+    document.write("Bienvenido al sitio");
+}
+if(edad < 18) {
+    document.write('no puedes igresar al sitio');
+}
